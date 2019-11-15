90150d371e08dc14a6731727f717f4565fe61d0d
ctslone <ctslone27@gmail.com>
2019-11-15 17:34:17 -0500

    adding working letter js file

---
+++ letter.js
@@ -0,0 +1,25 @@
+function Letter(letter) {
+    this.letter = letter.toString();
+    this.guessedYet = false;
+    this.letterResult = function () {
+        if (this.guessedYet === true) {
+            // console.log(this.letter)
+            return this.letter
+        }
+        else {
+            // console.log("_")
+            return "_"
+        }
+    };
+    this.letterChecker = function (letter) {
+        if (this.letter === letter) {
+            this.guessedYet = true;
+            // console.log("guessed")
+        }
+        // else {
+        //     console.log("nope")
+        // }
+    }
+}
+// var test = new Letter("A")
+// test.letterChecker()
