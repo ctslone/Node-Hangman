26955b8527c3f4d47f205bde5838e8c1bf1f8fbb
ctslone <ctslone27@gmail.com>
2019-11-21 16:15:08 -0500

    adding check to see if user whole word has been guessed. adding limit on number of guesses left and reset when user guesses are zero. adding color to the CLI logs

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var letter = require("./letter")
 var Word = require("./word")
 var inquirer = require("inquirer");
+var colors = require("colors")
 // pseudocode
 // starting global variables should include:
 // winning word
@@ -9,45 +10,111 @@ var inquirer = require("inquirer");
 // random words array
 
 var winningWord;
+var currentWord = "";
 var lettersGuessed = [];
 var guessesRemaining = 10;
-var allWords = ["hockey", "football"];
+var allWords = ["hockey", "football", "soccer", "basketball", "baseball", "wrestling", "rugby", "lacrosse", "tennis"];
 // invoke a start game function right off the bat to start the game
 // startgame function should pick a random word from a potential words array
 
 // need function to select a random word and make run it thriugh word.js
+newWord();
+startGame();
+
 function newWord() {
-    var currentWord = allWords[Math.floor(Math.random() * allWords.length)];
+    guessesRemaining = 10;
+    console.log("Try to guess the sport!")
+
+    currentWord = allWords[Math.floor(Math.random() * allWords.length)];
     console.log(currentWord)
-    
+
     winningWord = new Word(currentWord);
     winningWord.stringz()
-    // console.log(winningWord)
+    // console.log(winningWord.wordArray[0]) 
 }
-newWord()
 
+function wordGuessed() {
+    for (var i = 0; i < winningWord.wordArray.length; i++) {
+        if (!winningWord.wordArray[i].guessedYet) {
+            return false;
+        }
+    }
+    return true;
+}
 
 // start game/new game function 
 function startGame() {
 
-    inquirer.prompt([
-        {
-            type: "input",
-            name: "user_guess",
-            message: "Guess a letter: ",
-        }
-    ]).then(function (userRes) {
-        winningWord.guessCheck(userRes.user_guess)
-        winningWord.stringz()
-        if (winningWord.guessCheck(userRes.user_guess)) {
-            console.log("You guessed right! Keep it up!")
-            // startGame()
+    if (guessesRemaining === 0) {
+        console.log("You're out of guesses!".yellow + " Please try again!")
+        newWord()
+        startGame()
+    }
+
+    else {
+        if (!wordGuessed()) {
+            inquirer.prompt([
+                {
+                    type: "input",
+                    name: "user_guess",
+                    message: "Guess a letter: ",
+                }
+            ]).then(function (userRes) {
+                winningWord.guessCheck(userRes.user_guess)
+                winningWord.stringz()
+                // console.log(winningWord.wordArray)
+                if (winningWord.guessCheck(userRes.user_guess)) {
+                    console.log("You guessed right! Keep it up!".green)
+                    startGame()
+                }
+                else {
+                    guessesRemaining--
+                    console.log("Incorrect!".red + " You have " + guessesRemaining + " guesses left.")
+                    startGame()
+                }
+            })
         }
         else {
-            guessesRemaining--
-            console.log("Incorrect! You have " + guessesRemaining + " guesses left.")
-            // startGame()
+            console.log("Congrats! You win. Time for a new word.")
+            // allGuessed = true;
+            newWord()
+            startGame()
         }
-    })
+    }
+
+    
+
+
+    // var allGuessed = false;
+    // for (var i = 0; i < winningWord.wordArray.length; i++) {
+    //     if (winningWord.wordArray[i].guessedYet === false) {
+    //         allGuessed = false;
+    //     }
+    //     else {
+    //         allGuessed = true;
+    //     }
+    // }
+    // if (allGuessed === false) {
+
+    // }
+
+    
 }
-startGame();
+
+
+
+// function allLettersGuessed() {
+//     // console.log(winningWord.join(''))
+//     // winningWord.stringz() 
+//     // console.log(currentWord)
+//     // if (winningWord.join('') === currentWord) {
+
+//     // }
+//     for (var i = 0; i < winningWord.wordArray.length; i++) {
+//         if (winningWord.wordArray[i].guessedYet) {
+
+//         }
+//     }
+// }
+
+// allLettersGuessed()
