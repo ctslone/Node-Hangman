06073602fc69f17bd08cf0451710242e015a2059
ctslone <ctslone27@gmail.com>
2019-11-21 22:16:40 -0500

    adding more words to guess. adding and deleting comments. cleaning up code indentation. still have not added functionality to only allow a letter to be guessed once or only allow letters (not numbers) to be guessed.

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-var letter = require("./letter")
-var Word = require("./word")
-var inquirer = require("inquirer");
-var colors = require("colors")
-// pseudocode
 // starting global variables should include:
 // winning word
 // letters guessed array
 // guesses remaining
 // random words array
+
+var Word = require("./word")
+var inquirer = require("inquirer");
+var colors = require("colors")
+
 
 var winningWord;
 var currentWord = "";
@@ -16,24 +16,21 @@ var guessesRemaining = 10;
 var allWords = ["hockey", "football", "soccer", "basketball", "baseball", "wrestling", "rugby", "lacrosse", "tennis"];
 // invoke a start game function right off the bat to start the game
 // startgame function should pick a random word from a potential words array
-
-// need function to select a random word and make run it thriugh word.js
 newWord();
 startGame();
 
 function newWord() {
     guessesRemaining = 10;
     console.log("Try to guess the sport!")
-
     currentWord = allWords[Math.floor(Math.random() * allWords.length)];
-    console.log(currentWord)
-
+    // console.log(currentWord)
     winningWord = new Word(currentWord);
     winningWord.stringz()
     // console.log(winningWord.wordArray[0]) 
 }
 
 function wordGuessed() {
+    // check to see if all letters have been guessed yet, using the letter js file
     for (var i = 0; i < winningWord.wordArray.length; i++) {
         if (!winningWord.wordArray[i].guessedYet) {
             return false;
@@ -44,13 +41,13 @@ function wordGuessed() {
 
 // start game/new game function 
 function startGame() {
-
+    // if user out of guesses, pick a new word and start the game over
     if (guessesRemaining === 0) {
         console.log("You're out of guesses!".yellow + " Please try again!")
         newWord()
         startGame()
     }
-
+    // if user hasnt guessed word yet, keep prompting the user
     else {
         if (!wordGuessed()) {
             inquirer.prompt([
@@ -81,11 +78,11 @@ function startGame() {
             startGame()
         }
     }
+}
 
-    
+// TESTING CODE
 
-
-    // var allGuessed = false;
+// var allGuessed = false;
     // for (var i = 0; i < winningWord.wordArray.length; i++) {
     //     if (winningWord.wordArray[i].guessedYet === false) {
     //         allGuessed = false;
@@ -97,11 +94,6 @@ function startGame() {
     // if (allGuessed === false) {
 
     // }
-
-    
-}
-
-
 
 // function allLettersGuessed() {
 //     // console.log(winningWord.join(''))
